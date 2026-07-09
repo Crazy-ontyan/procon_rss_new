0a04f574fb229b56c21dae247ea207407798ac94
Robinson <wednesday.on.mys@icloud.com>
2026-07-09 15:39:40 +0900

    debug

--- procon_checker.py
+++ procon_checker.py
@@ -26,7 +26,8 @@ def get_latest_notice():
         if not title:
             continue
 
-        date_match = re.search(r'\d{4}/\d{2}/\d{2}', link_tag)
+        href_str = link_tag.get('href', '')
+        date_match = re.search(r'\d{4}/\d{2}/\d{2}', href_str)
         date = date_match.group(1) if date_match else "日付不明"
         print(date)
 
